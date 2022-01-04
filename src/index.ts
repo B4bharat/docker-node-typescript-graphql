3c0cacced0dedfefd2b0c70c528760bb7bd12e84
Bharat Poptwani <bharat.p@goodworker.in>
2022-01-04 13:03:25 +0530

    added fastify server config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+import Fastify from 'fastify';
+
+const fastify = Fastify({ logger: true });
+
+fastify.get('/', async () => {
+  return { hello: 'world' };
+});
+
+const port = process.env.PORT || 8080;
+const start = async () => {
+  try {
+    await fastify.listen(port, '0.0.0.0');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
