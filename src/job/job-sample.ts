6abda8fcc3b55450c0fa8bfac2cfeb160d7b1c25
Bharat Poptwani <bharat.p@goodworker.in>
2022-01-18 20:08:46 +0530

    connected job samples to dynamodb

--- src/job/job-sample.ts
+++ src/job/job-sample.ts
@@ -2,14 +2,11 @@ import { Job } from './job-type';
 import { fetchAllRecords } from '../libs/fetchAllRecords';
 import { unmarshall } from '@aws-sdk/util-dynamodb';
 
+let jobCollection: Job[] = [];
 (async () => {
   try {
     const jobs = await fetchAllRecords('job');
-    // from DynamoDB JSON
-    const unmarshalled = jobs.map((job: { [key: string]: any }) =>
-      unmarshall(job)
-    );
-    console.log('jobs', unmarshalled);
+    jobCollection = jobs.map((job: { [key: string]: any }) => unmarshall(job));
   } catch (e) {
     // Deal with the fact the chain failed
     console.error('dynamodb error', e);
@@ -17,50 +14,7 @@ import { unmarshall } from '@aws-sdk/util-dynamodb';
 })();
 
 export function createJobSamples() {
-  return [
-    createJob({
-      id: '1',
-      description: 'Desc 1',
-      title: 'Job 1',
-      wfh: true,
-      salary: 30000,
-      location: 'Location 1',
-      company: 'Company 1',
-      openPositions: 10,
-      minAge: 18,
-      maxAge: 30,
-      menAccomodation: true,
-      womenAccomodation: true,
-    }),
-    createJob({
-      id: '2',
-      description: 'Desc 2',
-      title: 'Job 2',
-      wfh: false,
-      salary: 40000,
-      location: 'Location 2',
-      company: 'Company 2',
-      openPositions: 20,
-      minAge: 18,
-      maxAge: 30,
-      menAccomodation: true,
-      womenAccomodation: true,
-    }),
-    createJob({
-      id: '3',
-      description: 'Desc 3',
-      title: 'Job 3',
-      wfh: true,
-      salary: 50000,
-      location: 'Location 3',
-      company: 'Company 3',
-      openPositions: 30,
-      minAge: 18,
-      maxAge: 30,
-      menAccomodation: true,
-      womenAccomodation: true,
-    }),
-  ];
+  return jobCollection.map((job) => createJob(job));
 }
 
 function createJob(jobData: Partial<Job>) {
