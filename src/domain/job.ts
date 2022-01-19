fb4b5f0fafc8e59164dc11c5963677acd3a31692
Bharat Poptwani <bharat.p@goodworker.in>
2022-01-19 16:14:05 +0530

    Added domain related inormation for Job

---
+++ src/domain/job.ts
@@ -0,0 +1,30 @@
+export interface Job {
+  id: string;
+  title: string;
+  wfh: boolean;
+  salary: number;
+  location: string;
+  company: string;
+  description: string;
+  openPositions: number;
+  minAge: number;
+  maxAge: number;
+  menAccomodation: boolean;
+  womenAccomodation: boolean;
+}
+
+export interface JobUseCase {
+  getJob(id: string): Promise<Job>;
+  getJobs(): Promise<Job[]>;
+  addJob(job: Job): Promise<Job>;
+  updateJob(job: Job): Promise<Job>;
+  deleteJob(id: string): Promise<Job>;
+}
+
+export interface JobRepository {
+  insert(job: Job): Promise<Job>;
+  findAll(): Promise<Job[]>;
+  findById(id: number): Promise<Job>;
+  updateContent(id: number, isDone: boolean): Promise<void>;
+  deleteById(id: number): Promise<void>;
+}
